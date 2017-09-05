bcdb711acb248918176f0a7eb56bb58b1fc9d681
River Gillis <jrivergillis@gmail.com>
2017-09-05 10:45:47 -0500

    add poor documentation for collision.h

--- collision.h
+++ collision.h
@@ -1,3 +1,8 @@
+// collision.h
+
+// Handles collision detection.
+// CheckCollisionWithPoints handles detection and correction (not very well).
+
 #ifndef COLLISION_H_
 #define COLLISION_H_
 
@@ -11,6 +16,8 @@ int CheckCollisionX(SDL_Rect a, SDL_Rect b);
 
 int CheckCollisionY(SDL_Rect a, SDL_Rect b);
 
+// Returns the pair of points to move @a to in order to no longer be colliding with @b
+//  vel_x, vel_y: the velocity of @a
 std::pair<int, int> CheckCollisionWithPoints(SDL_Rect a, SDL_Rect b, int vel_x, int vel_y);
 
 #endif
