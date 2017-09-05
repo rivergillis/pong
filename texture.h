0f24439d6b369e1ac67edc983afe6508912a36e0
River Gillis <jrivergillis@gmail.com>
2017-09-05 10:33:09 -0500

    Add documentation to texture.h

--- texture.h
+++ texture.h
@@ -1,3 +1,16 @@
+// texture.h
+// Author: River Gillis
+//
+// The main interface for loading, using, and rendering textures.
+// This is done through SDL_Textures.
+// Requires access to an SDL_Renderer in order to actually render the
+// textures.
+//
+// Usage:
+//	Texture texture(renderer);
+//	texture.LoadFromFile(file_name);
+//	texture.Render(x, y);
+
 #ifndef TEXTURE_H_
 #define TEXTURE_H_
 
@@ -10,38 +23,43 @@
 class Texture
 {
  public:
-	//Initializes variables
+	// Initializes variables
 	Texture();
 
-	// automatically inits
+	// Automatically calls Init()
 	Texture(SDL_Renderer* renderer, TTF_Font* font = nullptr);
 
-	//Deallocates memory
+	// Deallocates memory, calls Free()
 	~Texture();
 
 	void Init(SDL_Renderer* renderer, TTF_Font* font = nullptr);
 
-	//Loads image at specified path
+	// Loads image at specified path
 	bool LoadFromFile(const std::string& path);
 
 	#ifdef _SDL_TTF_H
-	//Creates image from font string
+	// Renders an image from a font string to the texture.
 	bool LoadFromRenderedText(const std::string& texture_text, SDL_Color text_color);
 	#endif
 
-	//Deallocates texture
+	// Deallocates @texture_
 	void Free();
 
-	//Set color modulation
+	// Set color modulation
 	void SetColor(Uint8 red, Uint8 green, Uint8 blue);
 
-	//Set blending
+	// Set blending
 	void SetBlendMode(SDL_BlendMode blending);
 
-	//Set alpha modulation
+	// Set alpha modulation
 	void SetAlpha(Uint8 alpha);
 
-	//Renders texture at given point
+	// Renders texture at given point
+	// 	x, y: The top-left point at which to render the texture
+	//	clip: The rectangle of the texture to render
+	//	angle: The angle of rotation to rotate the texture
+	//	center: The point at which rotation occurs
+	//	flip: The style of flipping for the texture
 	void Render(int x, int y, SDL_Rect* clip = nullptr, double angle = 0.0, SDL_Point* center = nullptr, SDL_RendererFlip flip = SDL_FLIP_NONE);
 
 	//Gets image dimensions
@@ -52,7 +70,7 @@ class Texture
 	SDL_Texture* GetPtr() { return texture_; }
 
  private:
-	//The actual hardware texture
+	// The actual SDL_Texture
 	SDL_Texture* texture_;
 
 	// The renderer we're using to render this texture
@@ -65,6 +83,7 @@ class Texture
 	int width_;
 	int height_;
 
+	// whether or not the texture has called Init()
 	bool has_init_;
 };
 
