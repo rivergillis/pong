e8f43abb04347bba099df5780fee6ccf80e2312c
River Gillis <jrivergillis@gmail.com>
2017-09-05 15:42:30 -0500

    Add little line of doc for utilities.h

--- utilities.h
+++ utilities.h
@@ -1,8 +1,14 @@
+// utilities.h
+// Author: River Gillis
+//
+// A collection of global utilities
+
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
 
 #include <SDL2/SDL.h>
 
+// returns true if a == b
 bool ColorsEqual(SDL_Color a, SDL_Color b);
 
 #endif 
